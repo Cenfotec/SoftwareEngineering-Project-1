2fe7a005e3b931fceeb5648ca27784943b721c88
Pablo Bonilla <pbonilla29@hotmail.com>
2018-02-03 17:37:11 -0600

    Updated Week 3
    Added "paginaSemantica v1.0" and "paginaSemantica v2.0"

---
+++ Classwork/Week 3/paginaSemantica v2.0/js/controlador.js
@@ -0,0 +1,14 @@
+let botonEnviar = document.querySelector('#btnEnviar');
+botonEnviar.addEventListener('click', obtenerDatos);
+
+function obtenerDatos() {
+  let inputNombre = document.querySelector('#txtNombre');
+  let sNombre = inputNombre.value;
+
+  let inputCorreo = document.querySelector('#txtCorreo');
+  let sCorreo = inputCorreo.value;
+
+  let inputTelefono = document.querySelector('#txtTelefono');
+  let sTelefono = inputTelefono.value;
+  console.log("Nombre: " + sNombre + "\nCorreo: " + sCorreo + "\nTelefono: " + sTelefono);
+}
