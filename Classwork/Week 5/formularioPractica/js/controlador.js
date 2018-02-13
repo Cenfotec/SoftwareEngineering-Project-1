624b8722e73c27a8f24562b081ca5181460dd915
Pablo Bonilla <pbonilla29@hotmail.com>
2018-02-13 11:41:06 -0800

    Added Week 5 and formularioPractica

---
+++ Classwork/Week 5/formularioPractica/js/controlador.js
@@ -0,0 +1,14 @@
+let btnAdd = document.querySelector('#btnAdd');
+
+btnAdd.addEventListener('click', addElement)
+
+function addElement() {
+  let sElementoQuimico = document.querySelector('#txtElementoQuimico').value;
+  console.log(sElementoQuimico);
+
+  let iPesoAtomico = document.querySelector("#txtPesoAtomico").value;
+  console.log(iPesoAtomico);
+
+  let sSimbolo = document.querySelector('#txtSimbolo').value;
+  console.log(sSimbolo);
+}
